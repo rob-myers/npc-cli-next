d86f7aaffd742e8cb8f7c657fb7154b51e1ace9a
robmyers <me.robmyers@gmail.com>
2024-09-18 15:26:19 +0100

    feat: static export, tested via `cd out && npx http-server`

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: 'export',
 };
 
 export default withMDX()(nextConfig);
