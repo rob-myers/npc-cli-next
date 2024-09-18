c972e64c7f99bec85d3df2b55f6dc8a0ae9b0c6b
robmyers <me.robmyers@gmail.com>
2024-09-18 20:55:01 +0100

    feat: add tabs css; add xterm css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 // https://nextjs.org/learn-pages-router/basics/assets-metadata-css/global-styles
 
 import "../src/components/globals.css";
-// import "flexlayout-react/style/light.css";
-// import "@xterm/xterm/css/xterm.css";
+import "flexlayout-react/style/light.css";
+import "@xterm/xterm/css/xterm.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
