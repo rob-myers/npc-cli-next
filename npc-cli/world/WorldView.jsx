55ff610826a4034f0f0ce800d4c068a113aad9ed
Rob Myers <me.robmyers@gmail.com>
2025-02-23 15:10:08 +0000

    feat: can zoom in further

--- npc-cli/world/WorldView.jsx
+++ npc-cli/world/WorldView.jsx
@@ -32,7 +32,7 @@ export default function WorldView(props) {
       maxAzimuthAngle: +Infinity,
       minPolarAngle: Math.PI * 0,
       maxPolarAngle: Math.PI * 1/3,
-      minDistance: 16,
+      minDistance: 8,
       maxDistance: 96,
       panSpeed: 2,
       zoomSpeed: 0.5,
