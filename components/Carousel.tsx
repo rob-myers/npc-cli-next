0c8e0f416b8db41db86623f6373ec5e132b396c9
robmyers <me.robmyers@gmail.com>
2025-04-07 14:42:08 +0100

    feat: refine carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -8,13 +8,13 @@ import { CarouselProvider, CarouselProviderProps, Slider, Slide, ButtonBack, But
 
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
-export default function Carousel2(props: Props) {
+export default function Carousel(props: Props) {
   return (
     <div css={carouselCss}>
       <CarouselProvider
         {...props}
-        naturalSlideWidth={500}
-        naturalSlideHeight={300}
+        naturalSlideWidth={props.naturalSlideWidth ?? props.aspectRatio ?? 16/9}
+        naturalSlideHeight={props.naturalSlideHeight ?? 1}
         totalSlides={2}
       >
         <Slider>
@@ -29,8 +29,8 @@ export default function Carousel2(props: Props) {
             </Slide>
           )}
         </Slider>
-        <ButtonBack>Back</ButtonBack>
-        <ButtonNext>Next</ButtonNext>
+        <ButtonBack>{'<'}</ButtonBack>
+        <ButtonNext>{'>'}</ButtonNext>
       </CarouselProvider>
     </div>
   );
@@ -38,6 +38,8 @@ export default function Carousel2(props: Props) {
 
 type Props = CarouselProviderProps & {
   // 🚧
+  /** Can use this instead of `naturalSlideWidth` and `naturalSlideHeight` */
+  aspectRatio?: number;
   slides: {
     img: StaticImageData;
     label?: string;
@@ -45,11 +47,36 @@ type Props = CarouselProviderProps & {
 };
 
 const carouselCss = css`
+
+  position: relative;
+
+  background-color: #999;
+
   .carousel__inner-slide {
     display: flex;
     align-items: center;
+    background-color: black;
   }
-  img {
-    /* margin: 0; */
+
+  .carousel__back-button, .carousel__next-button {
+    position: absolute;
+    top: 50%;
+    padding: 8px 24px;
+
+    line-height: 1.4;
+    color: white;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 2rem;
+    /* font-weight: 700; */
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: 8px;
+  }
+  .carousel__back-button {
+    left: 0;
+    margin-left: 24px;
+  }
+  .carousel__next-button {
+    right: 0;
+    margin-right: 24px;
   }
 `;
