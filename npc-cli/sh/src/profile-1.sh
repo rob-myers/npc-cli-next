86c26c4ba4cca7c3b36e0f4d7bfb6aaf2d233a01
Rob Myers <me.robmyers@gmail.com>
2025-04-26 14:13:36 +0100

    feat: skin: police-0 towards body-overlay

--- npc-cli/sh/src/profile-1.sh
+++ npc-cli/sh/src/profile-1.sh
@@ -29,6 +29,7 @@ spawn '{ npcKey: "policeman", angle: Math.PI, skin: {
   "head-{front,back,left,right,top,bottom}": { prefix: "police-0" },
   "head-overlay-{front,back,left,right,top,bottom}": { prefix: "police-0" },
   "body-overlay-{front,back,left,right,top,bottom}": { prefix: "police-0" },
+  "body-{front,back,left,right,top,bottom}": { prefix: "top-skin-only" },
 }}' '{ x: 1.5 * 1.5, y: 5 * 1.5 }'
 
 w n.rob.showSelector true
