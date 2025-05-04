c30141ffc6ab0180dc47ec667713a4f709901a02
Rob Myers <me.robmyers@gmail.com>
2025-05-04 10:59:13 +0100

    feat: profile-1: keep camera angle restrictions on mobile

--- npc-cli/sh/src/profile-1.sh
+++ npc-cli/sh/src/profile-1.sh
@@ -101,10 +101,12 @@ w update 'async w => {
 
 # fix camera angle and reduce maxDistance
 w update 'w => {
-  // w.view.ctrlOpts.minAzimuthAngle = Math.PI/4;
-  // w.view.ctrlOpts.maxAzimuthAngle = Math.PI/4;
-  // w.view.ctrlOpts.maxPolarAngle = Math.PI/4;
-  // w.view.ctrlOpts.maxDistance = 25;
+  if (w.smallViewport) {
+    w.view.ctrlOpts.minAzimuthAngle = Math.PI/4;
+    w.view.ctrlOpts.maxAzimuthAngle = Math.PI/4;
+    w.view.ctrlOpts.maxPolarAngle = Math.PI/4;
+    w.view.ctrlOpts.maxDistance = 25;
+  }
   w.floor.lit = true; // 🔔 lighting
 }'
 
