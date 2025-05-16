b196d7544884ab145b99f1ec83a03ec803eaecd3
Rob Myers <me.robmyers@gmail.com>
2025-05-16 21:46:01 +0100

    feat: try lower walls

--- npc-cli/service/const.js
+++ npc-cli/service/const.js
@@ -93,9 +93,9 @@ export const specialWallMetaKeys = /** @type {const} */ ([
   'hollow',
 ]);
 
-export const wallHeight = 2;
+export const wallHeight = 1.6;
 
-export const doorHeight = 1.6;
+export const doorHeight = 1.5;
 
 /** Depth of doorway along line walking through hull door */
 export const hullDoorDepth = 40 * sguToWorldScale * sguSymbolScaleDown;
