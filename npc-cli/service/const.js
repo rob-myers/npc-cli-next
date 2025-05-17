05dd4058cd30a2ce91349da8ae8b439bb18be6ec
Rob Myers <me.robmyers@gmail.com>
2025-05-17 08:10:44 +0100

    feat: back to higher walls

--- npc-cli/service/const.js
+++ npc-cli/service/const.js
@@ -93,9 +93,9 @@ export const specialWallMetaKeys = /** @type {const} */ ([
   'hollow',
 ]);
 
-export const wallHeight = 1.65;
+export const wallHeight = 2;
 
-export const doorHeight = 1.55;
+export const doorHeight = 1.6;
 
 /** Depth of doorway along line walking through hull door */
 export const hullDoorDepth = 40 * sguToWorldScale * sguSymbolScaleDown;
