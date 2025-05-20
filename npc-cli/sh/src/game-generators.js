b0133a3897b65a9418def3b76465bf651562c1dc
Rob Myers <me.robmyers@gmail.com>
2025-05-20 21:30:41 +0100

    feat: profile-1 zoom in more

--- npc-cli/sh/src/game-generators.js
+++ npc-cli/sh/src/game-generators.js
@@ -146,7 +146,7 @@ export async function* initCamAndLights({ api, args, w }) {
   if (npcKey in w.n) {
     w.view.lockDistance(); // prevent zoom-in while look
     await w.e.lookAt(npcKey).finally(() => w.view.unlockDistance());
-    await w.view.tween({ distance: 25 });
+    await w.view.tween({ distance: 12 });
   }
 
 }
