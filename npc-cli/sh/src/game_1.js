fe051eb4934237fef474d73a3b2ed297b55a5c57
Rob Myers <me.robmyers@gmail.com>
2025-06-22 09:54:14 +0100

    feat: fix types

--- npc-cli/sh/src/game_1.js
+++ npc-cli/sh/src/game_1.js
@@ -238,7 +238,7 @@ export const setupOnTickIdleTurn = ({ w, args }) => {
 export async function* tour(ct, opts = ct.api.jsArg(ct.args, { to: 'array' })) {
   const { api, lib } = ct;
   for (const to of opts.to) {// relax arrival dist
-    yield* lib.game.move(ct, { npcKey: opts.npcKey, to, s: { arriveDist: 0.1 } });
+    await lib.game.move(ct, { npcKey: opts.npcKey, to, s: { arriveDist: 0.1 } });
     await api.sleep(opts.pauseMs ?? 0.8);
   }
 }
