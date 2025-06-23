86d5c353741b2114bc7b954ace8cc5d90dd30f6d
Rob Myers <me.robmyers@gmail.com>
2025-06-23 08:02:16 +0100

    chore: turn off log

--- npc-cli/components/PsList.jsx
+++ npc-cli/components/PsList.jsx
@@ -66,7 +66,7 @@ export default function PsList() {
     disconnectSession: null,
     handleLeaderMessage(msg) {
       // 🚧
-      console.log(msg);
+      // console.log(msg);
     },
     onChangeSessionKey(e) {
       const { value } = e.currentTarget;
