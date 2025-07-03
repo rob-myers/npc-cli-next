17f51f84caa670cf54c6f68f99ef19d5e2608ecf
Rob Myers <me.robmyers@gmail.com>
2025-07-03 14:15:28 +0100

    feat: add icon--white-circle

--- npc-cli/service/const.js
+++ npc-cli/service/const.js
@@ -213,6 +213,7 @@ export const fromDecorImgKey = /** @type {const} */ ({// 🔔 must extend when a
   'icon--robot': true,
   'icon--square': true,
   'icon--warn': true,
+  'icon--white-circle': true,
   'screen--1x1': true,
 });
 
