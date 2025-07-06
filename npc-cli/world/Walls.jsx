89e73f6a1fe388dea6633f3b201fd6464a618a31
Rob Myers <me.robmyers@gmail.com>
2025-07-06 11:42:58 +0100

    feat: darker walls now we have opacityMin

--- npc-cli/world/Walls.jsx
+++ npc-cli/world/Walls.jsx
@@ -131,7 +131,7 @@ export default function Walls(props) {
         depthWrite={!transparent}
         transparent={transparent}
         opacity={state.opacity}
-        opacityCloseDivisor={20}
+        opacityCloseDivisor={10}
         opacityMin={0.5}
       />
     </instancedMesh>
