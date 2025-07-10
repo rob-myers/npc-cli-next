f20d92dfb8e5cc9c20c78db5ac0a5112c693fa04
Rob Myers <me.robmyers@gmail.com>
2025-07-10 10:04:22 +0100

    feat: Carousel: remove cube effect

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import type { Swiper as SwiperClass } from 'swiper/types';
 
-import { Scrollbar, EffectCube } from 'swiper/modules';
+import { Scrollbar } from 'swiper/modules';
 import { css } from '@emotion/react';
 
 import useStateRef from '@/npc-cli/hooks/use-state-ref';
@@ -12,7 +12,6 @@ import { mobileBreakpoint } from './const';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
-import 'swiper/css/effect-cube';
 
 export default function Carousel(props: Props) {
 
@@ -26,9 +25,8 @@ export default function Carousel(props: Props) {
     <Swiper
       // centeredSlides={props.items.length <= 2}
       css={carouselCss}
-      effect="cube"
       loop={false}
-      modules={[Scrollbar, EffectCube]}
+      modules={[Scrollbar]}
       onSwiper={state.onSwiper}
       scrollbar={{ draggable: true }}
       slidesPerView={1}
