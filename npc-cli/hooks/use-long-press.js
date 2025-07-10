df60e91f0c4869a0e5d85e17894557c410bccfe2
Rob Myers <me.robmyers@gmail.com>
2025-07-10 17:02:18 +0100

    feat: useLongPress: try fix again

--- npc-cli/hooks/use-long-press.js
+++ npc-cli/hooks/use-long-press.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { isTouchDevice } from "../service/dom";
 
 /**
  * - Based on https://stackoverflow.com/a/54749871/2917822
@@ -10,33 +11,31 @@ export default function useLongPress(config) {
   const ms = config.ms ?? 0;
   const timerId = React.useRef(-1);
   const epochMs = React.useRef(-1);
-  const touched = React.useRef(false);
 
   return React.useMemo(
     () => ({
-      onMouseDown() {
-        if (touched.current === true) return;
-        timerId.current = window.setTimeout(config.onLongPress, config.ms);
-        epochMs.current = Date.now();
-      },
-      onTouchStart() {
-        touched.current = true; // touch events now take precedence
-        timerId.current = window.setTimeout(config.onLongPress, config.ms);
-        epochMs.current = Date.now();
-      },
-      /** @param {React.MouseEvent} e */
-      onMouseUp(e) {
-        if (touched.current === true) return;
-        clearTimeout(timerId.current);
-        Date.now() - epochMs.current < ms && config.onClick?.(e);
-      },
-      /** @param {React.TouchEvent} e */
-      onTouchEnd(e) {
-        clearTimeout(timerId.current);
-        Date.now() - epochMs.current < ms && config.onClick?.(e);
+      ...isTouchDevice() ? {
+        onTouchStart() {
+          timerId.current = window.setTimeout(config.onLongPress, config.ms);
+          epochMs.current = Date.now();
+        },
+        /** @param {React.TouchEvent} e */
+        onTouchEnd(e) {
+          clearTimeout(timerId.current);
+          Date.now() - epochMs.current < ms && config.onClick?.(e);
+        },
+      } : {
+        onMouseDown() {
+          timerId.current = window.setTimeout(config.onLongPress, config.ms);
+          epochMs.current = Date.now();
+        },
+        /** @param {React.MouseEvent} e */
+        onMouseUp(e) {
+          clearTimeout(timerId.current);
+          Date.now() - epochMs.current < ms && config.onClick?.(e);
+        },
       },
       onMouseLeave() {
-        if (touched.current = true) return;
         clearTimeout(timerId.current);
       },
       /** @param {React.KeyboardEvent} e */
