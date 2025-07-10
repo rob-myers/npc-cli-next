bd2426bdccf064564bee92a731a427df85d6da91
Rob Myers <me.robmyers@gmail.com>
2025-07-10 16:06:15 +0100

    feat: ViewerControls: try fix mobile height 0

--- components/Viewer.tsx
+++ components/Viewer.tsx
@@ -255,9 +255,11 @@ const viewerCss = css`
 
   @media (max-width: ${breakpoint}) {
     flex-direction: column;
-    transition: min-height 500ms ease-in-out;
+    transition: height 500ms ease-in-out, min-height 500ms ease-in-out;
+    height: calc( max(var(${viewerBaseCssVar}), ${view.barSize}) );
     min-height: calc( max(var(${viewerBaseCssVar}), ${view.barSize}) );
     &.collapsed {
+      height: ${view.barSize};
       min-height: ${view.barSize};
     }
   }
