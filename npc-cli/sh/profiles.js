8e1f6dcae9e6303eed8ca6c33bf0c20186b66da0
Rob Myers <me.robmyers@gmail.com>
2025-07-30 09:42:14 +0100

    feat: use *.profile.sh suffix

--- npc-cli/sh/profiles.js
+++ npc-cli/sh/profiles.js
@@ -1,4 +1,4 @@
-export { default as default } from "./src/default_profile.sh";
-export { default as dev_only } from "./src/dev_only_profile.sh";
-export { default as empty } from "./src/empty_profile.sh";
-export { default as quickstart } from "./src/quickstart_profile.sh";
+export { default as default } from "./src/default.profile.sh";
+export { default as dev_only } from "./src/dev_only.profile.sh";
+export { default as empty } from "./src/empty.profile.sh";
+export { default as quickstart } from "./src/quickstart.profile.sh";
