d3853056ec60e7d90de3b75ef2dd595a2b87329f
Rob Myers <me.robmyers@gmail.com>
2025-08-03 08:23:53 +0100

    feat: change quickstart.profile skin

--- npc-cli/sh/src/quickstart.profile.sh
+++ npc-cli/sh/src/quickstart.profile.sh
@@ -5,8 +5,8 @@ source /etc/demo_1.js.sh
 
 awaitWorld
 
-# spawn rob with skeleton key
-spawn npcKey:rob at:'{x:4.5,y:7.5}' grant:.
+spawn npcKey:rob at:'{x:4.5,y:7.5}' \
+  skin:soldier-0,soldier-0 grant:.
 
 # click near nav to move
 ptags always && click meta.floor |
