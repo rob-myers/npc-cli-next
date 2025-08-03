54177612ab2be540f0356dd9cc670bd812b9b106
Rob Myers <me.robmyers@gmail.com>
2025-08-03 23:48:38 +0100

    feat: darker lit floor

--- npc-cli/service/glsl.js
+++ npc-cli/service/glsl.js
@@ -578,7 +578,7 @@ const instancedFloorShader = {
       
       if (showLights == true) {
         vec4 lightTexel = texture(lightAtlas, vec3(vUv, vTextureId));
-        float lighter = clamp(4.0 * lightTexel.w, 1.0, 3.0);
+        float lighter = clamp(2.5 * lightTexel.w, 1.0, 3.0);
         gl_FragColor = texel * vec4(vColor * diffuse * lighter, opacity) * 0.8;
       } else {
         gl_FragColor = texel * vec4(vColor * diffuse, opacity) * 2.0;
