d079d7c0f9666ac4b1f68121334013e75ccfef2a
Rob Myers <me.robmyers@gmail.com>
2025-08-08 16:19:44 +0100

    feat: temp default profile as dev_only

--- components/Viewer.tsx
+++ components/Viewer.tsx
@@ -172,7 +172,8 @@ export default function Viewer() {
     percentStr !== null && state.rootEl.style.setProperty(viewerCssVar.base, percentStr);
 
     // ensure layout if localStorage empty
-    useTabs.api.restoreLayoutWithFallback("world-tty-default", { preserveRestore: false });
+    // useTabs.api.restoreLayoutWithFallback("world-tty-default", { preserveRestore: false });
+    useTabs.api.restoreLayoutWithFallback("world-tty-dev_only", { preserveRestore: false });
 
     // handle #/internal/foo/bar triggered via links in blog
     function onHashChange() {
