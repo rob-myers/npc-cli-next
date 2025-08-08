344facaefd26608c3c67b9d448789bbaa5d9bedf
Rob Myers <me.robmyers@gmail.com>
2025-08-08 11:57:39 +0100

    feat: clean profiles + blog index

--- npc-cli/sh/src/quickstart.profile.sh
+++ npc-cli/sh/src/quickstart.profile.sh
@@ -1,10 +1,10 @@
-source /etc/util.sh
-source /etc/util.js.sh
-source /etc/game.js.sh
+source /etc/{util,game}.sh
+source /etc/{util,game}.js.sh
 source /etc/demo_1.js.sh
 
 awaitWorld
 
+# 🚧 simpler skin spec
 spawn npc:rob at:'{x:4.5,y:7.5}' \
   skin:soldier-0,soldier-0 grant:.
 
