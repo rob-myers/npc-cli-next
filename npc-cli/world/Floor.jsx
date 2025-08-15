fa13b14a6637de2be97d4ec1b0747cce006c3773
Rob Myers <me.robmyers@gmail.com>
2025-08-15 09:34:07 +0100

    feat: more colour restyling

--- npc-cli/world/Floor.jsx
+++ npc-cli/world/Floor.jsx
@@ -82,7 +82,7 @@ export default function Floor(props) {
       // nav
       const triangles = gm.navDecomp.tris.map(tri => new Poly(tri.map(i => gm.navDecomp.vs[i])));
       const navPoly = Poly.union(triangles.concat(gm.doors.map(x => x.computeDoorway())));
-      drawPolygons(ct, navPoly, ['#000a', '#999', 0.02]);
+      drawPolygons(ct, navPoly, ['#0009', '#889a', 0.02]);
 
       // grids
       ct.setTransform(1, 0, 0, 1, -gm.pngRect.x * worldToCanvas, -gm.pngRect.y * worldToCanvas);
