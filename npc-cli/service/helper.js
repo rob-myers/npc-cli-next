dda482bf856d1bbcf316e3e39721d5d5cd5e8ceb
Rob Myers <me.robmyers@gmail.com>
2025-08-19 18:43:46 +0100

    feat: faster walk

--- npc-cli/service/helper.js
+++ npc-cli/service/helper.js
@@ -140,7 +140,7 @@ export const helper = {
     height: npcClassToMeta[defaultClassKey].modelHeight * npcClassToMeta[defaultClassKey].scale,
     radius: npcClassToMeta[defaultClassKey].modelRadius * npcClassToMeta[defaultClassKey].scale * 0.675,
     runSpeed: npcClassToMeta[defaultClassKey].runSpeed * npcClassToMeta[defaultClassKey].scale * 0.9,
-    walkSpeed: npcClassToMeta[defaultClassKey].walkSpeed * npcClassToMeta[defaultClassKey].scale * 0.9,
+    walkSpeed: npcClassToMeta[defaultClassKey].walkSpeed * npcClassToMeta[defaultClassKey].scale * 1,
   },
 
   /** @type {Record<Key.Anim, true>} */
