187bd53c4da97902fbdf26787356b3673b41d834
Rob Myers <me.robmyers@gmail.com>
2025-08-19 08:31:46 +0100

    feat: smaller radius now scale is bigger

--- npc-cli/service/helper.js
+++ npc-cli/service/helper.js
@@ -138,7 +138,7 @@ export const helper = {
   /** 🚧 should be by classKey */
   defaults: {
     height: npcClassToMeta[defaultClassKey].modelHeight * npcClassToMeta[defaultClassKey].scale,
-    radius: npcClassToMeta[defaultClassKey].modelRadius * npcClassToMeta[defaultClassKey].scale * 0.75,
+    radius: npcClassToMeta[defaultClassKey].modelRadius * npcClassToMeta[defaultClassKey].scale * 0.675,
     runSpeed: npcClassToMeta[defaultClassKey].runSpeed * npcClassToMeta[defaultClassKey].scale * 0.9,
     walkSpeed: npcClassToMeta[defaultClassKey].walkSpeed * npcClassToMeta[defaultClassKey].scale * 0.9,
   },
