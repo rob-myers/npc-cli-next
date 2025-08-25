4aeb67c5b1ecab5a5dba2df1c4cf301c37dd7d05
Rob Myers <me.robmyers@gmail.com>
2025-08-25 16:22:58 +0100

    feat: brighter decor quads

--- npc-cli/world/Decor.jsx
+++ npc-cli/world/Decor.jsx
@@ -732,7 +732,7 @@ export default function Decor(props) {
       {ready && <instancedAtlasMaterial
         key={glsl.InstancedAtlasMaterial.key}
         alphaTest={0.5}
-        diffuse={[0.5, 0.5, 0.5]}
+        diffuse={[0.7, 0.7, 0.7]}
         atlas={w.texDecor.tex}
         objectPickRed={5}
         opacity={query.status === 'success' ? 1 : 0}
