6ca44a7e44c75d203b23372f6480b34f50b98511
Rob Myers <me.robmyers@gmail.com>
2025-09-12 16:06:10 +0100

    feat: larger separation weight for idle npcs in door sensor

--- npc-cli/world/use-handle-events.js
+++ npc-cli/world/use-handle-events.js
@@ -571,6 +571,14 @@ export default function useHandleEvents(w) {
           }
           break;
         }
+        case "stopped-moving": {
+          if (state.npcToDoors[e.npcKey]?.nearby.size > 0) {
+            // 🔔 try mitigate jerk onenter offMesh at maxSpeed
+            const npc = w.n[e.npcKey];
+            npc.agent?.raw.params.set_separationWeight(0.75);
+          }
+          break;
+        }
       }
     },
     improveOffMeshSrcDst(npc, offMesh) {
