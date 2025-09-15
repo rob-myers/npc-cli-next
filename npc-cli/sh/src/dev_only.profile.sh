1d91648198755da296de15d02893890212b7dbaa
Rob Myers <me.robmyers@gmail.com>
2025-09-15 16:26:18 +0100

    chore: clean

--- npc-cli/sh/src/dev_only.profile.sh
+++ npc-cli/sh/src/dev_only.profile.sh
@@ -4,11 +4,11 @@ import demoCameraWASD from demo
 
 awaitWorld
 
-spawn npc:rob as:soldier-0 at:'{x:6.5, y:5}' granted:. angle:Math.PI
-spawn npc:will as:scientist-0 at:'{ x: 2.5, y: 3 * 1.5 + 0.2 }' granted:. angle:Math.PI
-spawn npc:kate as:medic-0 at:'{ x: 4.5 * 1.5, y: 7 * 1.5 }' granted:. angle:Math.PI
-spawn npc:suit as:suit-0 at:'{ x: 0.5 * 1.5, y: 5 * 1.5 }' granted:. angle:Math.PI
-spawn npc:rada as:robot-1 at:'{ x: 1.5 * 1.5, y: 5 * 1.5 }' granted:. angle:Math.PI
+spawn npc:rob at:'{x:6.5, y:5}' as:soldier-0 granted:. angle:Math.PI
+spawn npc:will at:'{ x: 2.5, y: 3 * 1.5 + 0.2 }' as:scientist-0 granted:. angle:Math.PI
+spawn npc:kate at:'{ x: 4.5 * 1.5, y: 7 * 1.5 }' as:medic-0 granted:. angle:Math.PI
+spawn npc:suit at:'{ x: 0.5 * 1.5, y: 5 * 1.5 }' as:suit-0 granted:. angle:Math.PI
+spawn npc:rada at:'{ x: 1.5 * 1.5, y: 5 * 1.5 }' as:robot-1 granted:. angle:Math.PI
 
 npc rob api.showSelector true
 selected="rob"
