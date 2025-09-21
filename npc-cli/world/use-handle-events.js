5bb8fdee83182aeed31b2f314292a34ed6cff3cb
Rob Myers <me.robmyers@gmail.com>
2025-09-21 12:06:53 +0100

    feat: put findOtherBlockingNearDoor back

--- npc-cli/world/use-handle-events.js
+++ npc-cli/world/use-handle-events.js
@@ -90,6 +90,48 @@ export default function useHandleEvents(w) {
       state.doorToOffMesh[orig.gdKey] = state.doorToOffMesh[orig.gdKey].filter(x => x.npcKey !== npc.key);
       (state.npcToDoors[npc.key] ??= { inside: null, nearby: new Set() }).inside = null;
     },
+    findOtherBlockingNearDoor(npc, offMesh) {
+      const npcsNearbyDoor = state.doorToNearbyNpcs[offMesh.gdKey] ?? [];
+      const gmRoomId = /** @type {Geomorph.GmRoomId} */ (state.npcToRoom.get(npc.key));
+
+      for (const otherNpcKey of npcsNearbyDoor) {
+        if (otherNpcKey === npc.key) {
+          continue;
+        }
+
+        const other = w.n[otherNpcKey];
+        if (
+          state.npcToRoom.get(other.key)?.grKey !== gmRoomId.grKey // wrong room
+          || other.target !== null // handled elsewhere (?)
+        ) {
+          continue;
+        }
+
+        const otherIntersectsMainSeg = geom.lineSegCoordsIntersectsCircle(
+          offMesh.src.x, offMesh.src.z,
+          offMesh.dst.x, offMesh.dst.z,
+          other.point.x, other.point.y,
+          0.25,
+        );
+        
+        if (otherIntersectsMainSeg === false) {
+          continue; // other is not close enough to offMesh connection
+        }
+  
+        if (geom.lineSegCoordsIntersectsCircle(
+          npc.point.x,
+          npc.point.y,
+          offMesh.src.x, offMesh.src.z,
+          // offMesh.dst.x, offMesh.dst.z,
+          other.point.x, other.point.y,
+          .25,
+        ) === true) {
+          return otherNpcKey;
+        }
+      }
+  
+      return null;
+    },
     findOtherBlockingOppositeDir(offMesh, src, dst) {
       for (const tr of state.doorToOffMesh[offMesh.gdKey] ?? []) {
         if (tr.orig.srcGrKey === offMesh.srcGrKey) {
@@ -681,7 +723,11 @@ export default function useHandleEvents(w) {
         return;
       }
 
-      const blockingNpcKey = state.findOtherBlockingOppositeDir(offMesh, improved.src, improved.dst);
+      const blockingNpcKey = (
+        state.findOtherBlockingOppositeDir(offMesh, improved.src, improved.dst)
+        // 🚧 maybe do not check if "facing door" rather than "round the corner"
+        || state.findOtherBlockingNearDoor(npc, offMesh)
+      );
       if (blockingNpcKey !== null) {
         const lookAngleDst = npc.getLookAngle(improved.src);
         npc.stopMoving({
@@ -890,6 +936,10 @@ export default function useHandleEvents(w) {
  * @property {(npc: NPC.NPC, improved: NPC.ImprovedOffMeshSrcDst) => void} applyImprovedOffMesh
  * @property {(door: Geomorph.DoorState) => boolean} canCloseDoor
  * @property {(npc: NPC.NPC) => void} clearOffMesh
+* @property {(npc: NPC.NPC, offMesh: NPC.OffMeshLookupValue) => null | string} findOtherBlockingNearDoor
+ * offMesh early-exit-test i.e. test for some other npc which:
+ * - is idle and in the way
+ * - is very close to main segment of offMesh connection
  * @property {(offMesh: NPC.OffMeshLookupValue, src: Geom.VectJson, dst: Geom.VectJson) => null | string} findOtherBlockingOppositeDir
  * @property {(npcKey: string) => void} followNpc
  * @property {(npcKey: string, thoughtKey: string) => void} forget
