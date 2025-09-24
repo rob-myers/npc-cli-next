fb49a72f704fa953eea15b3fe82097caba94d2bc
Rob Myers <me.robmyers@gmail.com>
2025-09-24 21:35:18 +0100

    feat: try fixed polar (tilt) angle on desktop

--- npc-cli/world/CameraControls.jsx
+++ npc-cli/world/CameraControls.jsx
@@ -68,8 +68,8 @@ export const CameraControls = React.forwardRef(function CameraControls(props, re
       zoomToCursor
       minAzimuthAngle={-Infinity}
       maxAzimuthAngle={+Infinity}
-      minPolarAngle={isTouch ? Math.PI * 1/4 : 0}
-      maxPolarAngle={isTouch ? Math.PI * 1/4 : Math.PI * 1/4}
+      minPolarAngle={Math.PI / 4.5}
+      maxPolarAngle={Math.PI / 4.5}
       minDistance={props.minDistance} // target could be ground or npc head
       maxDistance={props.maxDistance}
       panSpeed={2}
