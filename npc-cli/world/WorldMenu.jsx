bb43619c92a05662cf40d4f5673c10f1fd2d7e07
Rob Myers <me.robmyers@gmail.com>
2025-10-03 22:28:37 +0100

    feat: dark by default; light floor has strong contrast

--- npc-cli/world/WorldMenu.jsx
+++ npc-cli/world/WorldMenu.jsx
@@ -28,7 +28,7 @@ export default function WorldMenu(props) {
   const state = useStateRef(/** @returns {State} */ () => ({
     bgScale: 8, // [1..20]
     brightness: tryLocalStorageGetParsed(`brightness@${w.key}`) ?? 12,
-    dark: false,
+    dark: true,
     defaultLoggerDim: { x: 0, y: 0, width: w.smallViewport ? 300 : 500, height: 100, minWidth: 200, minHeight: 80 },
     draggable: /** @type {*} */ (null),
     dragClassName: w.smallViewport ? popUpButtonClassName : undefined,
