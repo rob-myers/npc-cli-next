90fc14c7041b743d54461a753d2a7736379ea27f
Rob Myers <me.robmyers@gmail.com>
2025-10-05 12:56:41 +0100

    feat: npcs not so light in light mode

--- npc-cli/service/glsl.js
+++ npc-cli/service/glsl.js
@@ -168,7 +168,7 @@ const humanZeroShader = {
     } else {// body=1, breath=2, selector=3
 
       // 🌞 flat shading via vDotProduct
-      float ambientLight = dark ? 0.05 : 0.15;
+      float ambientLight = dark ? 0.05 : 0.1;
       tint *= vec4(vec3((ambientLight + 0.8 * vDotProduct) * vHeightShade), 1.0);
 
       // skinning
