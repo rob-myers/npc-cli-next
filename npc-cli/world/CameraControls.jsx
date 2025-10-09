95c7dc81807e6baa943ddc52b844e5163e2cb1b8
Rob Myers <me.robmyers@gmail.com>
2025-10-09 16:12:11 +0100

    feat: back to fixed polar angle

--- npc-cli/world/CameraControls.jsx
+++ npc-cli/world/CameraControls.jsx
@@ -68,8 +68,8 @@ export const CameraControls = React.forwardRef(function CameraControls(props, re
       zoomToCursor
       minAzimuthAngle={-Infinity}
       maxAzimuthAngle={+Infinity}
-      minPolarAngle={isTouch ? Math.PI / 4 : 0}
-      maxPolarAngle={isTouch ? Math.PI / 4 : Math.PI / 3}
+      minPolarAngle={Math.PI / 4}
+      maxPolarAngle={Math.PI / 4}
       minDistance={props.minDistance} // target could be ground or npc head
       maxDistance={props.maxDistance}
       panSpeed={2}
