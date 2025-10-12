e90fa3f12abf4f7aec43b18ab2fa83e2100c2aae
Rob Myers <me.robmyers@gmail.com>
2025-10-12 22:45:46 +0100

    feat: tweak profile

--- npc-cli/sh/src/narrate_to_bed.profile.sh
+++ npc-cli/sh/src/narrate_to_bed.profile.sh
@@ -6,9 +6,15 @@ awaitWorld
 
 w decor.showLabels true
 
-spawn npc:rob at:'{x:2.5, y:3*1.5+0.2}' as:soldier-0 granted:. angle:Math.PI
-look at:rob
-w e.followNpc rob '{ fixAngle: false, smoothTime: 1, maxDistance: 7.5, fromBehind: false }'
+spawn npc:rob at:'{x:6.5, y:5}' as:soldier-0 granted:. angle:Math.PI
+spawn npc:will at:'{ x: 2.5, y: 3 * 1.5 + 0.2 }' as:scientist-0 granted:. angle:Math.PI
+spawn npc:kate at:'{ x: 4.5 * 1.5, y: 7 * 1.5 }' as:medic-0 granted:. angle:Math.PI
+spawn npc:suit at:'{ x: 0.5 * 1.5, y: 5 * 1.5 }' as:suit-0 granted:. angle:Math.PI
+spawn npc:rada at:'{ x: 1.5 * 1.5, y: 5 * 1.5 }' as:robot-1 granted:. angle:Math.PI
+
+# look at:rob
+w view.controls.setPolarAngle Math.PI/4
+w e.followNpc rob "{ fixedAngle: true, smoothTime: 1, maxDistance: 10, fromBehind: false }"
 
 permitMove=true
 click '({ meta }, ct) => meta.floor && ct.home.permitMove' |
