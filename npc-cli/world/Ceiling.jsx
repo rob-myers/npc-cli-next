9f7901c0924d3716f882adfa3b51f85a87ba16bf
Rob Myers <me.robmyers@gmail.com>
2025-10-25 22:01:43 +0100

    feat: less transparent ceilings

--- npc-cli/world/Ceiling.jsx
+++ npc-cli/world/Ceiling.jsx
@@ -136,7 +136,7 @@ export default function Ceiling(props) {
         opacity={state.opacity}
         objectPickRed={3}
         opacityCloseDivisor={10}
-        opacityMin={0.5}
+        opacityMin={0.9}
       />
     </instancedMesh>
   );
