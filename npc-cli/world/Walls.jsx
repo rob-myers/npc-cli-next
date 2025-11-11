af7200c4fc016a055a973dac8f3f97d12c973122
Rob Myers <me.robmyers@gmail.com>
2025-11-11 08:49:53 +0000

    feat: more transparent to account for larger minDistance

--- npc-cli/world/Walls.jsx
+++ npc-cli/world/Walls.jsx
@@ -136,7 +136,7 @@ export default function Walls(props) {
         depthWrite={!transparent}
         transparent={transparent}
         opacity={state.opacity}
-        opacityCloseDivisor={12}
+        opacityCloseDivisor={20}
         opacityMin={state.opacity}
       />
     </instancedMesh>
