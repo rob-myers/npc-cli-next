042a4ff0fbc4fcf5e711e5dfedb4f0d50b8dd721
Rob Myers <me.robmyers@gmail.com>
2025-11-15 21:29:58 +0000

    feat: fov and small viewport tweaks

--- npc-cli/world/Walls.jsx
+++ npc-cli/world/Walls.jsx
@@ -136,7 +136,7 @@ export default function Walls(props) {
         depthWrite={!transparent}
         transparent={transparent}
         opacity={state.opacity}
-        opacityCloseDivisor={25}
+        opacityCloseDivisor={20}
         opacityMin={state.opacity}
       />
     </instancedMesh>
--- npc-cli/world/WorldView.jsx
+++ npc-cli/world/WorldView.jsx
@@ -28,7 +28,7 @@ export default function WorldView(props) {
   const w = React.useContext(WorldContext);
 
   const state = useStateRef(/** @returns {State} */ () => ({
-    camInitPos: [0, 35, 0],
+    camInitPos: [0, w.smallViewport ? 18 : 35, 0],
     canvas: /** @type {*} */ (null),
     clickIds: [],
     controls: /** @type {*} */ (null),
