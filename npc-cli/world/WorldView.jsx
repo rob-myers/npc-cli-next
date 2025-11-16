18681d183e7a4ce3ce7211090c25c769dcbe34a0
Rob Myers <me.robmyers@gmail.com>
2025-11-16 09:27:10 +0000

    feat: start closer on desktop

--- npc-cli/world/WorldView.jsx
+++ npc-cli/world/WorldView.jsx
@@ -28,7 +28,7 @@ export default function WorldView(props) {
   const w = React.useContext(WorldContext);
 
   const state = useStateRef(/** @returns {State} */ () => ({
-    camInitPos: [0, w.smallViewport ? 18 : 35, 0],
+    camInitPos: [0, w.smallViewport ? 18 : 30, 0],
     canvas: /** @type {*} */ (null),
     clickIds: [],
     controls: /** @type {*} */ (null),
